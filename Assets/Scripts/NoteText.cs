33422e6c12a4569224692bceabe47500a57d0570
gcwhitfield <gwhitfieldjr@gmail.com>
2022-05-20 12:02:19 -0400

    Update Note UI

---
+++ Assets/Scripts/NoteText.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Note Text", menuName = "Dialogue/New Note Dialogue")]
+public class NoteText : ScriptableObject
+{
+    public string text;
+}
